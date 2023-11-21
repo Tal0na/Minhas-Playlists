72e87be9d88256215c62fce7fa808ebbede8e707
Tales Costa <talonacontato@gmail.com>
2023-11-21 11:06:08 -0300

    Add files via upload

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -14,71 +14,98 @@ import {
 const Main = () => {
   return (
     <>
-      <div className="grid grid-cols-5 grid-rows-6 gap-2 m-2">
-        <div className="p-4 rounded bg-zinc-900 items-center justify-center">
-          <FaHome size={30} />
-          Ínicio
-          <FaSearch size={30} />
-          Pesquisar
-          {1}
+      <div className="grid grid-cols-5 grid-rows-7 gap-2 m-2">
+        <div className="p-4 rounded bg-zinc-900 flex flex-col justify-center">
+          <div className="flex items-center mb-2">
+            <div className="mr-2 text-gray-400 hover:text-white transition-colors duration-300 ease-in-out flex items-center">
+              <FaHome size={30} className="mr-2" />
+              <span>Ínicio</span>
+            </div>
+          </div>
+          <div className="flex items-center mt-4">
+            <div className="mr-2 text-gray-400 hover:text-white transition-colors duration-300 ease-in-out flex items-center">
+              <FaSearch size={30} className="mr-2" />
+              <span>Pesquisar</span>
+            </div>
+          </div>
         </div>
-        <div className="row-span-4 col-start-1 row-start-2 p-4 rounded bg-zinc-900 flex flex-col overflow-auto">
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
+        <div
+          className="row-span-4 col-start-1 row-start-2 p-4 rounded bg-zinc-900 flex flex-col 
+        overflow-auto"
+        >
+          <div className="text-lg">
+            MInha Biblioteca
           </div>
+          <div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/74N2GGm7zgRZB0Wqt98wS0" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">01 I Pop</h2>
+  </a>
+  <p>Descrição</p>
+</div>
 
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
 
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/0RbqVCzXkLeyz6Z0dZriwl" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">02 I Pop Alternative</h2>
+  </a>
+  <p>Descrição</p>
+</div>
 
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/4WAAsJEtVnS4qzLz6ttohC" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">03 I Rap/Hip Hop</h2>
+  </a>
+  <p>Descrição</p>
+</div>
 
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
-          <div>
-            <h2 className="text-xl font-bold">Título</h2>
-            <p>Descrição</p>
-          </div>
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/6sYubCIaWsuP4CSfjxMTGW" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">04</h2>
+  </a>
+  <p>Descrição</p>
+</div>
+
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/6RTieNiLPLWm2xalN7xcLd" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">05</h2>
+  </a>
+  <p>Descrição</p>
+</div>
+
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/2VfLADNvEDyf2D46nd8Hfv" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">06</h2>
+  </a>
+  <p>Descrição</p>
+</div>
+
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/1ip1N8nCRpfxu0rGLXKye7" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">07</h2>
+  </a>
+  <p>Descrição</p>
+</div>
+
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/0DSAsUte2KDCj4B5qBiawh" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">08</h2>
+  </a>
+  <p>Descrição</p>
+</div>
+
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/2UD65Tc0eP1eAGFjHN6ei5" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">09</h2>
+  </a>
+  <p>Descrição</p>
+</div>
+
+<div className="hover:bg-zinc-800 p-4 rounded">
+  <a href="https://open.spotify.com/playlist/3oPMxSlnoTLKMbfd7Pck9j" target="_blank" rel="noopener noreferrer">
+    <h2 className="text-xl ">Título</h2>
+  </a>
+  <p>Descrição</p>
+</div>
           {2}
         </div>
 
---
+++ src/app/components/old/mainold.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+<div className="h-screen flex flex-col">
+        <div className="flex flex-1">
+          <aside className="w-72 bg-zinc-950 rouded-lg p-6">
+            <nav>
+              <a
+                href=""
+                className="flex items-center gap-2 text-xl font-semibold text-zinc-100"
+              >
+                Home
+              </a>
+              <a
+                href=""
+                className="flex items-center gap-2 text-xl font-semibold text-zinc-100"
+              >
+                Search
+              </a>
+            </nav>
+
+            <nav className="mt-10 flex flex-col gap-2">
+              <a href="" className="text-lg ">
+                1
+              </a>
+            </nav>
+          </aside>
+          <main className="fleX-1 P-6">
+            <div className="flex items-center gap-4">
+              <button className="rounded-full bg-black/40 p-1">b1</button>
+              <button className="rounded-full bg-black/40 p-1">b2</button>
+            </div>
+            <h1 className="font-semibold text-3xl mt-10">Good Afternoom</h1>
+
+            <div className="grid grid-cols-3 gap-4 mt-4">
+              <a
+                href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden 
+                hover:bg-white/20 transition-colors">
+                <image  width={104} height={104} alt="j" />
+                <strong>Musica</strong>
+                <button className="w-12 h-12 flex items-center pl-1 ml-auto">
+                  play
+                </button>
+              </a>
+            </div>
+          </main>
+        </div>
+        <footer className="bg-black p-10">footer</footer>
+      </div>
